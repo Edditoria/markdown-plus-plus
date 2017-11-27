a1bf41a50bfe70f302aed52aeb00483c6b4df1c1
Edditoria <edditoria@gmail.com>
2017-11-27 20:23:09 +0800

    [chore] update build scripts to create both builds using one command; details:
    - Usage is rolled back to original: `source build.sh [theme-name]`
    - Now it will create 2 XML files: one for classic build and one for modern build
    - The `function make_file` now accepts argument: "modern" or "classic"
    - The scripts of checking file existence is moved into `function make_file`
    - Variables "template_file" and "output_file" are moved into `function make_file`

--- build.sh
+++ build.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
 function print_help {
-  echo "Usage: source build.sh [modern | classic] [theme-name]"
+  echo "Usage: source build.sh [theme-name]"
   echo "Build UDL xml file in root directory of this repo."
   echo ""
-  echo "Examples: . build.sh modern awesome-black"
-  echo "          . build.sh classic lite-default"
+  echo "Examples: . build.sh awesome-black"
+  echo "          . build.sh lite-default"
   echo ""
   echo "* This script requires mustache.js (npm install -g mustache)"
   echo ""
 }
 function make_file {
-  mustache $data_file $template_file > $output_file
-  echo "File is created: $output_file"
+  # argument $1: "modern" or "classic"
+  build_version="$1"
+  template_file="./build/userDefinedLang-markdown.template.${build_version}.xml"
+  output_file="./userDefinedLang-markdown.${theme_name}.${build_version}.xml"
+  echo "Creating file: ${output_file}..."
+  if [ -f $output_file ]; then
+    # when output file is already existed
+    read -p "File already existed. Overwrite? [y|N] " answer
+    if [[ $answer =~ ^(Y|y|Yes|yes) ]]; then
+      mustache $data_file $template_file > $output_file
+      echo "File is overwritten"
+    else
+      echo "Aborted."
+    fi
+  else
+    mustache $data_file $template_file > $output_file
+    echo "File is created"
+  fi
 }
 
 if [ $# -eq 0 ] || [[ $1 =~ ^("help"|"--help"|"-h"|"h") ]]; then
@@ -20,12 +36,9 @@ if [ $# -eq 0 ] || [[ $1 =~ ^("help"|"--help"|"-h"|"h") ]]; then
   print_help
 else
   # setup variables
-  build_version="$1"
-  theme_name="$2"
+  theme_name="$1"
   data_file="./theme-${theme_name}/data.${theme_name}.json"
   template_data="./build/data.template.json"
-  template_file="./build/userDefinedLang-markdown.template.${build_version}.xml"
-  output_file="./userDefinedLang-markdown.${theme_name}.${build_version}.xml"
 
   # main part of this script
   if [ ! -f $data_file ]; then
@@ -46,15 +59,8 @@ else
       echo ""
       print_help
     fi
-  elif [ -f $output_file ]; then
-    # when output file is already existed
-    read -p "File already existed. Overwrite? [y|N] " answer
-    if [[ $answer =~ ^(Y|y|Yes|yes) ]]; then
-      make_file
-    else
-      echo "Aborted."
-    fi
   else
-    make_file
+    make_file modern
+    make_file classic
   fi
 fi
