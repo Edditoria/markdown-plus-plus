362b15bac2638aaff06fb184536b6da3c3f3d38c
Edditoria <edditoria@gmail.com>
2018-12-18 19:34:03 +0800

    Remove build.ps1 script; Update build.sh using the version in master branch. Details:
    The current build.ps1 scripts cannot handle true-false templating, that is implementing in master branch.
    
    The build script breaks now, but will work again right before merging.

--- build.sh
+++ build.sh
@@ -1,64 +1,66 @@
 #!/bin/bash
 
 function print_help {
-  echo "Usage: source build.sh [theme_name]"
-  echo "Build UDL xml file in root directory of this repo."
-  echo ""
-  echo "Example      : . build.sh deep_black"
-  echo "Display help : . build.sh help"
-  echo ""
-  echo "* This script requires mustache.js (npm install -g mustache)"
-  echo ""
+	echo "Usage: source build.sh [theme-name]"
+	echo "Build UDL xml file in root directory of this repo."
+	echo ""
+	echo "Examples: . build.sh awesome-black"
+	echo "          . build.sh lite-default"
+	echo ""
+	echo "* This script requires mustache.js (npm install -g mustache)"
+	echo ""
 }
 function make_file {
-  mustache $data_file $template_file > $output_file
-  echo "File is created: $output_file"
+	# argument $1: "modern" or "classic"
+	build_version="$1"
+	template_file="./build/userDefinedLang-markdown.template.${build_version}.xml"
+	output_file="./userDefinedLang-markdown.${theme_name}.${build_version}.xml"
+	echo "Creating file: ${output_file}..."
+	if [ -f $output_file ]; then
+		# when output file is already existed
+		read -p "File already existed. Overwrite? [y|N] " answer
+		if [[ $answer =~ ^(Y|y|Yes|yes) ]]; then
+			mustache $data_file $template_file > $output_file
+			echo "File is overwritten"
+		else
+			echo "Aborted."
+		fi
+	else
+		mustache $data_file $template_file > $output_file
+		echo "File is created"
+	fi
 }
 
 if [ $# -eq 0 ] || [[ $1 =~ ^("help"|"--help"|"-h"|"h") ]]; then
-  # when arg is help or empty
-  print_help
+	# when arg is help or empty
+	print_help
 else
-  # setup variables
-  theme_name="$1"
-  data_file="./${theme_name}_theme/${theme_name}_data.json"
-  template_file="./build/build_template.xml"
-  template_data="./build/build_data_template.json"
-  if [ $1 == "default" ]; then
-    output_file="./${theme_name}_theme/userDefineLang_markdown.xml"
-  else
-    output_file="./${theme_name}_theme/userDefineLang_markdown_${theme_name}.xml"
-  fi
-  
-  # main part of this script
-  if [ ! -f $data_file ]; then
-    # when $data_file is missing
-    echo "Cannot find the source or directory."
-    echo ""
-    read -p "Are you going to create a new color scheme \"${theme_name}\"? [y|N] " answer
-    if [[ $answer =~ ^(Y|y|Yes|yes) ]]; then
-      printf "Creating folder..."
-      mkdir ./${theme_name}_theme
-      echo "[done]"
-      printf "Creating template json file..."
-      cp ${template_data} ${data_file}
-      echo "[done]"
-      echo ""
-    else
-      echo "Aborted."
-      echo ""
-      print_help
-    fi
-  elif [ -f $output_file ]; then
-    # when output file is already existed
-    read -p "File already existed. Overwrite? [y|N] " answer
-    if [[ $answer =~ ^(Y|y|Yes|yes) ]]; then
-      make_file
-    else
-      echo "Aborted."
-    fi
-  else
-    make_file
-  fi
-fi
+	# setup variables
+	theme_name="$1"
+	data_file="./theme-${theme_name}/data.${theme_name}.json"
+	template_data="./build/data.template.json"
 
+	# main part of this script
+	if [ ! -f $data_file ]; then
+		# when $data_file is missing
+		echo "Cannot find the source or directory."
+		echo ""
+		read -p "Are you going to create a new color scheme \"${theme_name}\"? [y|N] " answer
+		if [[ $answer =~ ^(Y|y|Yes|yes) ]]; then
+			printf "Creating folder..."
+			mkdir "./theme-${theme_name}"
+			echo "[done]"
+			printf "Creating template json file..."
+			cp ${template_data} ${data_file}
+			echo "[done]"
+			echo ""
+		else
+			echo "Aborted."
+			echo ""
+			print_help
+		fi
+	else
+		make_file modern
+		make_file classic
+	fi
+fi
