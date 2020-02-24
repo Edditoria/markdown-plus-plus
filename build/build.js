ebcda44371f883c514418f59345108e69cc3932e
Edditoria <edditoria@gmail.com>
2020-02-24 19:19:29 +0800

    Improve paths, template function,render() and JSDoc comments
    - `paths` has full paths of this npm package, config, udl and template
    - `template()` is now generated by a function called `createTemplate()`
    - `render()` takes "template" function as an argument
    - Better log by showing theme name when doing `render()`
    - The file object is unified. It has the properties of:
      - Full path of config file
      - Full path of UDL output file
      - Theme name

--- build/build.js
+++ build/build.js
@@ -1,81 +1,97 @@
 /**
- * Markdown-plus-plus is a project of markdown syntax highlighting for Notepad++, by customized UDL file (user defined language)
- * @file Build script for markdown-plus-plus
+ * Markdown-plus-plus is a project of markdown syntax highlighting for Notepad++, by customized UDL file (user defined language).
+ * This project is open source on:
+ * [Source]{@link https://github.com/Edditoria/markdown-plus-plus}.
+ * Code released under the MIT license:
+ * [License]{@link https://github.com/Edditoria/validid/blob/master/LICENSE.txt}
+ *
+ * @file Build script for markdown-plus-plus.
  * @auther Edditoria
  * @license MIT
- * Code released under the MIT license:
- * https://github.com/Edditoria/validid/blob/master/LICENSE.txt
  */
 
 var fs = require('fs');
 var Handlebars = require('handlebars');
 var path = require('path');
 
+var packagePath = path.resolve(__dirname, '../');
+
 /**
  * @typedef {Object} Paths
- * @description Collection of paths for this npm package
- * @property {string} package - Root directory of this npm package
- * @property {string} template - File path of Handlebars template
+ * @description Collection of paths for this npm package.
+ * @property {string} package - Root directory of this npm package.
+ * @property {string} config - Directory for config files.
+ * @property {string} udl - Directory for expected UDL XML files.
+ * @property {string} template - File path of Handlebars template.
  */
 /** @type {Paths} paths */
 var paths = {
-	package: path.resolve(__dirname, '../'),
-	template: path.resolve(__dirname, 'template.hbs.xml')
+	package: packagePath,
+	config: packagePath + '/config',
+	udl: packagePath + '/udl',
+	template: packagePath + '/build/template.hbs.xml'
 };
 
 /**
- * Create a function of Handlebars-template. Requires paths object to run.
- * @function template
+ * Create a function of Handlebars template.
+ * @param {string} templatePath - Full path of template file.
+ * @return {Object} - A function of Handlebars template.
  */
-var template = Handlebars.compile(
-	// Read template file synchronously
-	// Making it a string is required by Handlebars
-	fs.readFileSync(paths.template).toString()
-);
+var createTemplate = function(templatePath) {
+	return Handlebars.compile(
+		// Read template file synchronously.
+		// Making it a string is required by Handlebars.
+		fs.readFileSync(templatePath).toString()
+	);
+};
 
 /**
- * Render Handlebars template in Node 4 or above. Requires template() to run
- * @param {Files} files - The {@link Files} object for Handlebars rendering
+ * @typedef {Object} Files
+ * @description Full paths of config data, expected output UDL files and etc.
+ * @property {string} config - Full path of config file
+ * @property {string} udl - Full path of expected UDL file
+ * @property {string} themeName - In format of dash-lower-case-theme-name
  */
-var render = function (files) {
+/**
+ * Render and write all files using Handlebars template.
+ * @param {Files} files - The {@link Files} object for Handlebars rendering.
+ */
+var render = function(files, template) {
 	/* @function readFile - Read the config file asynchronously */
-	fs.readFile(files.data, 'utf8', function(dataError, data) {
+	fs.readFile(files.config, 'utf8', function(dataError, data) {
 		if (!dataError) {
 			var dataObj = JSON.parse(data);
 			var output = template(dataObj);
 			/* @function writeFile - Write the file asynchronously */
-			fs.writeFile(files.output, output, function(writeError) {
+			fs.writeFile(files.udl, output, function(writeError) {
 				if (!writeError) {
-					console.log('Write file successfully');
+					console.log('[' + files.themeName + '] UDL file is created successfully');
 				} else {
-					console.log('[fs.writeFile] Error in saving file');
+					console.log('[' + files.themeName + '] Error in saving UDL file');
 					throw writeError;
 				}
 			});
 		} else {
-			console.log('[fs.readFile] Error in loading template');
+			console.log('[' + files.themeName + '] Error in loading config data');
 			throw dataError;
 		}
 	});
 };
 
 /**
- * @typedef {Object} Files
- * @description Full paths of config data, expected output UDL files and etc.
- * @property {string} data - Full path of config data
- * @property {string} output - Full path of expected UDL file
- */
-/**
  * Create a Files object in format of {@link Files}
  * @param {string} themeName - In format of dash-lower-case-theme-name
  * @param {Paths} paths - The {@link Paths} object for Handlebars rendering
  * @return {Files}
  */
-var createFilesObj = function (themeName, paths) {
+var createFilesObj = function(themeName, paths) {
 	return {
-		data: paths.package + '/config/markdown.' + themeName + '.config.json',
-		output: paths.package + '/udl/markdown.' + themeName + '.udl.xml'
+		config: paths.config + '/markdown.' + themeName + '.config.json',
+		udl: paths.udl + '/markdown.' + themeName + '.udl.xml',
+		themeName: themeName
 	};
 };
 
-render(createFilesObj('default', paths));
+var template = createTemplate(paths.template);
+var defaultFiles = createFilesObj('default', paths);
+render(defaultFiles, template);
