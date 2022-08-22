4875f9300c49c9a5e60aa89e1fbeb1c90559c4aa
Edditoria <edditoria@pm.me>
2022-08-23 05:04:41 +0800

    temp: Add prettier configs

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+	useTabs: true,
+	singleQuote: true,
+	jsxSingleQuote: false, // as default.
+	trailingComma: 'none',
+	bracketSpacing: true, // as default.
+	printWidth: 100
+};
